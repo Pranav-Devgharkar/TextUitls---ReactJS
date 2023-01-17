ba7d38f3b8f816119782cca034e6604ec5afbca9
Pranav-Devgharkar <3415prnv@gmail.com>
2023-01-17 20:57:12 +0530

    Updates

---
+++ precache-manifest.9055517492770fc06a78a66107898ba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2031cad5cfdd480577f2",
+    "url": "/TextUitls---ReactJS/static/css/main.92ca017e.chunk.css"
+  },
+  {
+    "revision": "2031cad5cfdd480577f2",
+    "url": "/TextUitls---ReactJS/static/js/main.2031cad5.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TextUitls---ReactJS/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUitls---ReactJS/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "2c0bc8418e016cec92f5",
+    "url": "/TextUitls---ReactJS/static/js/runtime~main.2c0bc841.js"
+  },
+  {
+    "revision": "a69f6674244c463d788e04acfecbd38a",
+    "url": "/TextUitls---ReactJS/index.html"
+  }
+];
